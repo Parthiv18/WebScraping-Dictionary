67ac78db9db1aeeaf50a33a5afe474af5625266e
Parthiv18 <56895655+Parthiv18@users.noreply.github.com>
2021-06-06 21:37:43 -0400

    Test

---
+++ findWord/src/findWord/sWord.java
@@ -0,0 +1,43 @@
+package findWord;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class sWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print ("Enter Word ");
+		Scanner scan = new Scanner (System.in);
+		String word = scan.nextLine ();
+		gWords (word);
+	}
+		
+	public static void gWords (String word)
+		{
+		    try
+		    { //System.out.print(word);
+		        Document doc = Jsoup.connect ("https://www.google.com/search?q=" + word + "+meaning").timeout (6000).get ();
+		        Elements temp = doc.select ("div.L1jWkf.h3TRxf"); //for dictionay - default-content - lister-item-content - css-1avshm7 e16867sm0 - css-1ghs5zt e1q3nk1v2   
+		        
+		        int i = 0;
+		        for (Element element:
+		        temp)
+		        {
+		            i++;
+		            System.out.print (i + " " + element.select ("span").first ().text () + "\n"); //first() //if (i==3) { //break;   //}
+		        }
+		    }
+
+
+		    catch (IOException e)
+		    {
+		        e.printStackTrace ();
+		    }
+
+}
---
+++ findWord/src/module-info.java
@@ -0,0 +1,3 @@
+module findWord {
+	requires org.jsoup;
+}
