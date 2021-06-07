ad276a701e793ca7aa04865a6070768b86bd5813
Parthiv18 <56895655+Parthiv18@users.noreply.github.com>
2021-06-07 17:24:34 -0400

    Updated links

--- findWord/src/findWord/sWord.java
+++ findWord/src/findWord/sWord.java
@@ -24,13 +24,13 @@ public class sWord {
 		    { 
 		    	//System.out.print(word);
 		        Document doc = Jsoup.connect ("https://www.merriam-webster.com/dictionary/"+word).timeout (6000).get ();
-		        Elements temp = doc.select ("div.sense.has-sn"); //for dictionay - default-content - lister-item-content - css-1avshm7 e16867sm0 - css-1ghs5zt e1q3nk1v2   
+		        Elements temp = doc.select ("div.vg"); //for dictionay - default-content - lister-item-content - css-1avshm7 e16867sm0 - css-1ghs5zt e1q3nk1v2   
 		        
 		        int i = 0;
 		        for (Element element:temp)
 		        {
 		            i++;
-		            System.out.print (i+" "+element.select("span.dtText").first().text()+ "\n"); //first() //if (i==3) { //break;   //}
+		            System.out.print (i+" "+element.select("span.dt").first().text()+ "\n"); //first() //if (i==3) { //break;   //}
 		        }
 		    }
 		    catch (IOException e)
